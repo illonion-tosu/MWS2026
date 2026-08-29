32966443fbed0c355ffc059a8fc8ad0936f75878
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2026-08-29 19:31:21 +1000

    feat: complete display

--- seeding/index.js
+++ seeding/index.js
@@ -15,6 +15,21 @@ const playerPfpEl = document.getElementById("player-pfp")
 const playerFlagEl = document.getElementById("player-flag")
 const playerNameEl = document.getElementById("player-name")
 const playerRankEl = document.getElementById("player-rank")
+const playerSeedEl = document.getElementById("player-seed")
+const mods = [
+    {
+        mod: "nm",
+        count: 5
+    },
+    {
+        mod: "hr",
+        count: 3
+    },
+    {
+        mod: "dt",
+        count: 3
+    }
+]
 
 function displayStats() {
     const currentPlayer = players[playerCounter]
@@ -23,4 +38,17 @@ function displayStats() {
     playerFlagEl.style.backgroundImage = `https://osuflags.omkserver.nl/${currentPlayer.player_flag}-200.png`
     playerNameEl.textContent = currentPlayer.player_name
     playerRankEl.textContent = `#${currentPlayer.player_rank.toLocaleString()}`
+    playerSeedEl.textContent = `#${currentPlayer.player_seed.toLocaleString()}`
+
+    // Mod ranks
+    for (let i = 0; i < mods.length; i++) {
+        const currentMod = mods[i]
+        const modName = currentMod.mod
+        document.getElementById(`${modName}-rank`).textContent = currentPlayer[`${modName}_rank`]
+
+        for (let j = 0; j < currentMod.count; j++) {
+            document.getElementById(`${modName}${j + 1}`).children[1].textContent = currentPlayer[`${modName}${j + 1}_score`].toLocaleString()
+            document.getElementById(`${modName}${j + 1}`).children[2].textContent = currentPlayer[`#${modName}${j + 1}_rank`].toLocaleString()
+        }
+    }
 }
