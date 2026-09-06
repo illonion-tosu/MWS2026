74428a57912f403b1e8a3d704358fd3a266bc7e1
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2026-09-06 10:53:59 +1000

    feat: add video

--- schedule/index.js
+++ schedule/index.js
@@ -1,5 +1,13 @@
 import { loadBeatmaps } from "../_shared/core/beatmaps.js"
 
+// Autoplay video
+const videoEl = document.getElementById("video")
+
+document.addEventListener("click", () => {
+    videoEl.muted = false
+    videoEl.play()
+}, { once: true })
+
 // DOM Elements
 const elements = {
     countdown: {
